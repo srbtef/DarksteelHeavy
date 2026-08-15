fdb68297ce34140b756562e0d610085c33042d86
srbtef <awatrdhg@outlook.com>
2026-08-15 18:01:20 +0800

    第一次提交修改

--- src/example/Test.java
+++ src/example/Test.java
@@ -1,14 +1,33 @@
 package example;
 
-import example.content.block.ACoreBlock;
-import mindustry.mod.Mod;
-import mindustry.world.blocks.storage.CoreBlock;
-import mindustry.world.meta.BlockGroup;
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
 
-public class Test extends Mod{
-    public static CoreBlock acore;
-@Override
+public class ExampleJavaMod extends Mod{
+
+    public ExampleJavaMod(){
+        Log.info("Loaded ExampleJavaMod constructor.");
+
+        //listen for game load event
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("behold").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
     public void loadContent(){
-        acore = new ACoreBlock("acore");
-    } 
+        Log.info("Loading some example content.");
+    }
+
 }
