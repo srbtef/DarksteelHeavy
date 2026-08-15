3dfc07d10ba045cb1ff805a84a754aa6946f1e91
srbtef <awatrdhg@outlook.com>
2026-08-15 18:32:38 +0800

    极简可拆核心

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -6,7 +6,6 @@ import mindustry.mod.Mod;
 public class ExampleJavaMod extends Mod {
     @Override
     public void loadContent(){
-        Test democore = new Test("democore");
-        Vars.content.register(democore);
+        Vars.content.add(new Test("democore"));
     }
 }
--- src/example/Test.java
+++ src/example/Test.java
@@ -1,8 +1,5 @@
 package src.example;
 
-import mindustry.Vars;
-import mindustry.gen.FloatingItem;
-import mindustry.gen.Unit;
 import mindustry.world.blocks.storage.CoreBlock;
 
 public class Test extends CoreBlock {
@@ -12,27 +9,7 @@ public class Test extends CoreBlock {
         size = 3;
         health = 5000;
         itemCapacity = 5000;
-        // 开启可拆卸
+        // 开启可拆卸权限
         destructible = true;
-        // 拆卸掉落自身对应物品
-        itemDrop = Vars.content.item(name);
-    }
-
-    @Override
-    public void setStats(){
-        super.setStats();
-    }
-
-    public class TestBuild extends CoreBuild{
-
-        // 拆卸完成回调，必须带Unit参数匹配父类签名
-        @Override
-        public void onDeconstructed(Unit deconstructor){
-            super.onDeconstructed(deconstructor);
-            // 生成掉落物
-            if(block.itemDrop != null){
-                FloatingItem.create(block.itemDrop, tile.worldx(), tile.worldy()).add();
-            }
-        }
     }
 }
