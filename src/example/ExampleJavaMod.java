15e16e7ec5bd5c2e64a6e9e30526b2331f9b097e
srbtef <awatrdhg@outlook.com>
2026-08-15 18:45:13 +0800

    核心方块全地形可放置

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,11 +1,34 @@
 package src.example;
 
-import mindustry.Vars;
+import mindustry.game.Team;
 import mindustry.mod.Mod;
+import mindustry.world.Tile;
+import mindustry.world.blocks.storage.CoreBlock;
 
 public class ExampleJavaMod extends Mod {
     @Override
     public void loadContent(){
-        Vars.content.add(new Test("democore"));
+        new Test("democore").register();
+    }
+}
+
+class Test extends CoreBlock {
+    public Test(String name){
+        super(name);
+        size = 3;
+        health = 5000;
+        itemCapacity = 5000;
+        destructible = true;
+    }
+
+    @Override
+    public boolean canBreak(Tile tile){
+        return true;
+    }
+
+    @Override
+    public boolean canPlaceOn(Tile tile, Team team, int rotation){
+        // 移除所有地形、核心重叠限制，任意位置均可放置
+        return tile != null;
     }
 }
