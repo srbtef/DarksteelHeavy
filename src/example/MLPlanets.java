3f701ccfc8c4ce80be7ee0d02ae5a8c7826f4bb2
srbtef <awatrdhg@outlook.com>
2026-08-17 22:26:31 +0800

    修复包名，使用blocks集合注册

--- src/example/MLPlanets.java
+++ src/example/MLPlanets.java
@@ -1,70 +1,64 @@
-package example;
+package magical.content;
 
-import arc.graphics.Color;
-import arc.math.geom.Vec3;
-import arc.util.noise.Simplex;
-import mindustry.content.Blocks;
 import mindustry.content.Planets;
-import mindustry.graphics.g3d.HexMesh;
-import mindustry.graphics.g3d.HexSkyMesh;
-import mindustry.graphics.g3d.MultiMesh;
-import mindustry.graphics.g3d.PlanetGrid.Ptile;
-import mindustry.maps.generators.PlanetGenerator;
-import mindustry.type.Planet;
-import mindustry.world.TileGen;
+import arc.func.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.game.*;
+import mindustry.graphics.*;
+import mindustry.graphics.g3d.*;
+import mindustry.graphics.g3d.PlanetGrid.*;
+import mindustry.maps.planet.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+import mindustry.world.blocks.environment.*;
+
 
 public class MLPlanets {
     public static Planet cecilia;
 
-    public static class CeciliaGenerator extends PlanetGenerator {
-        @Override
-        public void genTile(Vec3 position, TileGen tile) {
-            float rawNoise = Simplex.noise2d(1, position.x, position.y, 6, 0.5f, 1f / 75f);
-            float height = (rawNoise + 1f) / 2f;
-
-            if (height > 0.60f) {
-                tile.floor = Blocks.stone;
-                tile.block = Blocks.air;
-            } else if (height > 0.45f) {
-                tile.floor = Blocks.sand;
-                tile.block = Blocks.air;
-            } else {
-                tile.floor = Blocks.ice;
-                tile.block = Blocks.air;
-            }
-        }
-    }
-
     public static void load() {
-        cecilia = new Planet("cecilia", Planets.sun, 1f, 3) {{
-            generator = new CeciliaGenerator();
 
-            meshLoader = () -> new MultiMesh(
-                new HexMesh(this, 6),
-                new HexSkyMesh(this, 2, 0.15f, 0.14f, 5,
-                        Color.valueOf("97B5EDFF").a(0.75f), 2, 0.42f, 1f, 0.43f),
-                new HexSkyMesh(this, 3, 0.6f, 0.15f, 5,
-                        Color.valueOf("97B5EDFF").a(0.70f), 2, 0.42f, 1.2f, 0.45f)
+         cecilia = new Planet("cecilia", Planets.sun, 1f, 3) {{
+            generator = new SerpuloPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 6);
+            cloudMeshLoader = () -> new MultiMesh(
+                    new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("97B5EDFF").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                    new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("97B5EDFF").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
             );
 
-            localizedName = "塞西莉亚";
-            orbitRadius = 40f;
-            rotateTime = 3000f;
+            launchCapacityMultiplier = 0f;
             sectorSeed = 1;
-            startSector = 1;
-
-            alwaysUnlocked = true;
-            accessible = true;
-            drawOrbit = true;
-            hasAtmosphere = true;
-
+             tidalLock = false;
+             alwaysUnlocked = true;
+             clearSectorOnLose = true;
+             showRtsAIRule = false;
+             allowCampaignRules = true;
+             allowLegacyLaunchPads = false;
+             allowWaves = true;
+             allowLaunchLoadout = false;
+             visible = true;
+             drawOrbit = true;
+             accessible = true;
+             hasAtmosphere = true;
+             updateLighting = true;
+             allowLaunchToNumbered = false;
+             allowSectorInvasion = true;
+             bloom = false;
+             allowLaunchSchematics = true;
             iconColor = Color.valueOf("97B5EDFF");
             atmosphereColor = Color.valueOf("97B5EDFF");
             atmosphereRadIn = 0.02f;
             atmosphereRadOut = 0.3f;
-
-            //直接添加Ptile，去掉new Sector
-            sectors.add(new Ptile(0, 0));
+            orbitRadius = 40;
+            rotateTime = 3000;
+            startSector = 1;
+            alwaysUnlocked = true;
+            landCloudColor = Pal.spore.cpy().a(0.5f);
         }};
     }
 }
