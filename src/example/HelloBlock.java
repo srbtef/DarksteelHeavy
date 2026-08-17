57f199212f35275c7b78bd589451e03e2be265dc
srbtef <awatrdhg@outlook.com>
2026-08-17 17:39:58 +0800

    修复包名，使用blocks集合注册

--- src/example/HelloBlock.java
+++ src/example/HelloBlock.java
@@ -1,32 +1,18 @@
-package com.example.mymod;
+package example;
 
-import arc.graphics.*;
-import arc.scene.ui.*;
-import arc.scene.ui.layout.*;
-import arc.util.*;
-import mindustry.gen.*;
-import mindustry.type.*;
-import mindustry.world.*;
-import mindustry.world.meta.*;
+import arc.scene.ui.Dialog;
+import mindustry.content.Items;
+import mindustry.gen.Building;
+import mindustry.type.ItemStack;
+import mindustry.world.Block;
+import mindustry.world.meta.Category;
 
 import static mindustry.Vars.*;
 
 public class HelloBlock extends Block {
 
-    // 自定义的 Building 类
-    public class HelloBuild extends Building {
-        @Override
-        public void tapped() {
-            // 玩家点击方块时触发
-            if (this.tap != null) {
-                showHelloDialog();
-            }
-        }
-    }
-
     public HelloBlock() {
         super("hello-block");
-
         localizedName = "问候方块";
         description = "点击我，我会向你问好。";
         health = 100;
@@ -34,21 +20,26 @@ public class HelloBlock extends Block {
         solid = true;
         update = true;
         hasPower = false;
-        category = Category.units;  // 可改为其他分类
-        buildType = HelloBuild::new;  // 指定自定义 Building
+        category = Category.units;
         requirements(Category.units, ItemStack.with(Items.copper, 10));
+        buildType = HelloBuild::new;
     }
 
-    public void load() {
-        // 注册方块到游戏中
-        content.blocks().add(this);
+    public class HelloBuild extends Building {
+        @Override
+        public void tapped() {
+            showHelloDialog();
+        }
     }
 
     private void showHelloDialog() {
-        // 创建一个简单对话框
         Dialog dialog = new Dialog("你好");
         dialog.cont.add("你好，世界！").pad(20f);
         dialog.addCloseButton();
         dialog.show();
+    }
+
+    public void load() {
+        content.blocks().add(this);
     }
 }
