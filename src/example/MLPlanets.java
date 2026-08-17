01f26f2fc14be50466a9cf613cdd651076a05200
srbtef <awatrdhg@outlook.com>
2026-08-17 22:47:11 +0800

    修复包名，使用blocks集合注册

--- src/example/MLPlanets.java
+++ src/example/MLPlanets.java
@@ -1,64 +1,67 @@
 package example;
 
+import arc.graphics.Color;
+import arc.math.geom.Vec3;
+import arc.util.noise.Simplex;
+import mindustry.content.Blocks;
 import mindustry.content.Planets;
-import arc.func.*;
-import arc.graphics.*;
-import arc.math.*;
-import arc.math.geom.*;
-import arc.struct.*;
-import arc.util.*;
-import mindustry.game.*;
-import mindustry.graphics.*;
-import mindustry.graphics.g3d.*;
-import mindustry.graphics.g3d.PlanetGrid.*;
-import mindustry.maps.planet.*;
-import mindustry.type.*;
-import mindustry.world.*;
-import mindustry.world.meta.*;
-import mindustry.world.blocks.environment.*;
-
+import mindustry.graphics.g3d.HexMesh;
+import mindustry.graphics.g3d.HexSkyMesh;
+import mindustry.graphics.g3d.MultiMesh;
+import mindustry.graphics.g3d.PlanetGrid.Ptile;
+import mindustry.maps.generators.PlanetGenerator;
+import mindustry.type.Planet;
+import mindustry.world.TileGen;
 
 public class MLPlanets {
     public static Planet cecilia;
 
-    public static void load() {
+    public static class CeciliaGenerator extends PlanetGenerator {
+        @Override
+        public void genTile(Vec3 position, TileGen tile) {
+            float rawNoise = Simplex.noise2d(1, position.x, position.y, 6, 0.5f, 1f / 75f);
+            float height = (rawNoise + 1f) / 2f;
 
-         cecilia = new Planet("cecilia", Planets.sun, 1f, 3) {{
-            generator = new SerpuloPlanetGenerator();
-            meshLoader = () -> new HexMesh(this, 6);
-            cloudMeshLoader = () -> new MultiMesh(
-                    new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("97B5EDFF").a(0.75f), 2, 0.42f, 1f, 0.43f),
-                    new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("97B5EDFF").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
+            if (height > 0.60f) {
+                tile.floor = Blocks.stone;
+                tile.block = Blocks.air;
+            } else if (height > 0.45f) {
+                tile.floor = Blocks.sand;
+                tile.block = Blocks.air;
+            } else {
+                tile.floor = Blocks.ice;
+                tile.block = Blocks.air;
+            }
+        }
+    }
+
+    public static void load() {
+        cecilia = new Planet("cecilia", Planets.sun, 1f, 3) {{
+            generator = new CeciliaGenerator();
+
+            meshLoader = () -> new MultiMesh(
+                new HexMesh(this, 6),
+                new HexSkyMesh(this, 2, 0.15f, 0.14f, 5,
+                        Color.valueOf("97B5EDFF").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                new HexSkyMesh(this, 3, 0.6f, 0.15f, 5,
+                        Color.valueOf("97B5EDFF").a(0.70f), 2, 0.42f, 1.2f, 0.45f)
             );
 
-            launchCapacityMultiplier = 0f;
+            localizedName = "塞西莉亚";
+            orbitRadius = 40f;
+            rotateTime = 3000f;
             sectorSeed = 1;
-             tidalLock = false;
-             alwaysUnlocked = true;
-             clearSectorOnLose = true;
-             showRtsAIRule = false;
-             allowCampaignRules = true;
-             allowLegacyLaunchPads = false;
-             allowWaves = true;
-             allowLaunchLoadout = false;
-             visible = true;
-             drawOrbit = true;
-             accessible = true;
-             hasAtmosphere = true;
-             updateLighting = true;
-             allowLaunchToNumbered = false;
-             allowSectorInvasion = true;
-             bloom = false;
-             allowLaunchSchematics = true;
+            startSector = 1;
+
+            alwaysUnlocked = true;
+            accessible = true;
+            drawOrbit = true;
+            hasAtmosphere = true;
+
             iconColor = Color.valueOf("97B5EDFF");
             atmosphereColor = Color.valueOf("97B5EDFF");
             atmosphereRadIn = 0.02f;
             atmosphereRadOut = 0.3f;
-            orbitRadius = 40;
-            rotateTime = 3000;
-            startSector = 1;
-            alwaysUnlocked = true;
-            landCloudColor = Pal.spore.cpy().a(0.5f);
         }};
     }
 }
--- src/example/MyMod.java
+++ src/example/MyMod.java
@@ -1,24 +1,13 @@
-package example.mymod;
+package example;
 
-import arc.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
-import example.SimpleCore;
+import mindustry.mod.Mod;
+import example.MLPlanets;
+import example.SimpleCore
 
-public class MyMod extends Mod {
-
-    public MyMod() {
-        Log.info("Loaded MyJavaMod constructor.");
-    }
-
+public class MyMod extends Mod{
     @Override
     public void loadContent() {
-        Log.info("Loading content.");
-        new SimpleCore();
         MLPlanets.load();
+        SimpleCore.load();
     }
 }
