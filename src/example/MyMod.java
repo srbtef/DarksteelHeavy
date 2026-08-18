09589f3758886673286b395b5814c3745a17cab9
srbtef <awatrdhg@outlook.com>
2026-08-18 21:45:50 +0800

    修复包名，使用blocks集合注册

--- src/example/MyMod.java
+++ src/example/MyMod.java
@@ -2,10 +2,12 @@ package example;
 
 import mindustry.mod.Mod;
 import example.MLPlanets;
+import example.ExampleCoreBlock;
 
 public class MyMod extends Mod{
     @Override
     public void loadContent() {
         MLPlanets.load();
+        ExampleCoreBlock.load();
     }
 }
