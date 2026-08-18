97407330ae895a8e67aa1e5311e20188a13c70fa
srbtef <awatrdhg@outlook.com>
2026-08-18 21:27:20 +0800

    修复包名，使用blocks集合注册

---
+++ src/example/MLPlanets.java
@@ -0,0 +1,63 @@
+package example;
+
+import mindustry.content.Planets;
+import arc.func.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.game.*;
+import mindustry.graphics.*;
+import mindustry.graphics.g3d.*;
+import mindustry.graphics.g3d.PlanetGrid.*;
+import mindustry.maps.planet.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+import mindustry.world.blocks.environment.*;
+
+public class MLPlanets {
+    public static Planet cecilia;
+
+    public static void load() {
+
+         cecilia = new Planet("cecilia", Planets.sun, 1f, 3) {{
+            generator = new SerpuloPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 6);
+            cloudMeshLoader = () -> new MultiMesh(
+                    new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("97B5EDFF").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                    new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("97B5EDFF").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
+            );
+
+            launchCapacityMultiplier = 0f;
+            sectorSeed = 1;
+             tidalLock = false;
+             alwaysUnlocked = true;
+             clearSectorOnLose = true;
+             showRtsAIRule = false;
+             allowCampaignRules = true;
+             allowLegacyLaunchPads = false;
+             allowWaves = true;
+             allowLaunchLoadout = false;
+             visible = true;
+             drawOrbit = true;
+             accessible = true;
+             hasAtmosphere = true;
+             updateLighting = true;
+             allowLaunchToNumbered = false;
+             allowSectorInvasion = true;
+             bloom = false;
+             allowLaunchSchematics = true;
+            iconColor = Color.valueOf("97B5EDFF");
+            atmosphereColor = Color.valueOf("97B5EDFF");
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            orbitRadius = 40;
+            rotateTime = 3000;
+            startSector = 1;
+            alwaysUnlocked = true;
+            landCloudColor = Pal.spore.cpy().a(0.5f);
+        }};
+    }
+}
--- src/example/MyMod.java
+++ src/example/MyMod.java
@@ -1,12 +1,13 @@
 package example;
 
 import mindustry.mod.Mod;
-import example.fPlanets;
+import example.MLPlanets;
 import example.SimpleCore;
 
 public class MyMod extends Mod{
     @Override
     public void loadContent() {
-        fPlanets.load();
+        MLPlanets.load();
+        SimpleCorw.load();
     }
 }
