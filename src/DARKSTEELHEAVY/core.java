4f05ab8f330ef8480521f4cfeed66e61215f90df
srbtef <awatrdhg@outlook.com>
2026-08-24 21:09:37 +0800

    修复包名，使用blocks集合注册

--- src/DARKSTEELHEAVY/core.java
+++ src/DARKSTEELHEAVY/core.java
@@ -1,20 +1,28 @@
-package mindustry.content;
+package DARKSTEELHEAVY;
 
-import mindustry.*;
-import mindustry.game.*;
-import mindustry.type.*;
-import mindustry.world.blocks.storage.*;
-import mindustry.world.meta.*;
+import mindustry.type.ItemStack;
+import mindustry.world.blocks.storage.CoreBlock;
+import mindustry.world.meta.BuildVisibility;
+import mindustry.game.Category;
+import mindustry.type.UnitType;
+import mindustry.content.Items;
+import mindustry.content.UnitTypes;
 
-coreShard = new CoreBlock("core-23"){{
+public class CoreContent {
+
+    public static CoreBlock coreShard;
+
+    public static void load() {
+        coreShard = new CoreBlock("core-23"){{
+            requirements(Category.effect, BuildVisibility.coreZoneOnly, ItemStack.with(Items.copper, 1000, Items.lead, 800));
             alwaysUnlocked = true;
-
             isFirstTier = true;
             unitType = UnitTypes.alpha;
             health = 1100;
             itemCapacity = 4000;
             size = 3;
             buildCostMultiplier = 2f;
-
             unitCapModifier = 8;
         }};
+    }
+}
