ee28388fadb8ff1db2f9f411bb4f847d3663b71f
srbtef <awatrdhg@outlook.com>
2026-08-25 14:48:05 +0800

    修复包名，使用blocks集合注册

---
+++ src/darksteel/items.java
@@ -0,0 +1,13 @@
+package darksteel
+
+import mindustry.game.Item;
+import mindustry.graphics.Color;
+
+public class XuanSteelItem extends Item {
+    public XuanSteelItem(String name) {
+        super(name);
+        this.color = Color.valueOf("#708090");
+        this.hardness = 3;
+        this.stackSize = 50;
+    }
+}
