43c1d9e553eaf44b06fe0e26e36880dd77d777aa
srbtef <awatrdhg@outlook.com>
2026-08-26 21:07:47 +0800

    修复包名，使用blocks集合注册

--- src/darksteel/content/Blocks.java
+++ src/darksteel/content/Blocks.java
@@ -1,58 +1,29 @@
-package magical.content;
+package darksteel.content;
 
-import arc.Core;
-import arc.graphics.Color;
-import arc.graphics.g2d.Draw;
-import arc.graphics.g2d.Fill;
-import arc.graphics.g2d.Lines;
-import arc.math.Angles;
-import arc.math.Interp;
-import arc.math.Mathf;
-import arc.math.Rand;
-import arc.math.geom.Vec2;
-import arc.util.Time;
-import arc.util.Tmp;
-import arc.util.io.Reads;
-import arc.util.io.Writes;
-import mindustry.Vars;
-import mindustry.world.Block;
+import mindustry.world.blocks.storage.CoreBlock;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
-import mindustry.gen.Building;
+import mindustry.content.Items;
+import mindustry.content.UnitTypes;
 import mindustry.world.meta.BuildVisibility;
-import mindustry.world.meta.Stat;
-import mindustry.world.blocks.units;
-import mindustry.world.meta.StatUnit;
-import mindustry.world.draw.DrawBlock;
-import mindustry.world.draw.DrawDefault;
-import mindustry.world.draw.DrawMulti;
-import mindustry.world.draw.DrawRegion;
-import mindustry.world.draw.DrawTurret;
-import mindustry.entities.part.RegionPart;
-import mindustry.entities.part.HaloPart;
-import mindustry.entities.Effect;
-import arc.struct.Seq;
-import darksteel.content.unit;
+import mindustry.world.Block;
 
 public class Blocks {
-
-    public static Block
-            //基础科技
-            baseCor;
+    // 修正变量名统一
+    public static Block baseCor;
 
     public static void load() {
-                coreShard = new CoreBlock("core-shard"){{
-            requirements(Category.effect, BuildVisibility.coreZoneOnly, with(Items.copper, 1000, Items.lead, 800));
+        baseCor = new CoreBlock("core-shard"){{
+            // 补全ItemStack.with，修复requirements参数
+            requirements(Category.effect, BuildVisibility.coreZoneOnly, ItemStack.with(Items.copper, 1000, Items.lead, 800));
             alwaysUnlocked = true;
-
             isFirstTier = true;
             unitType = UnitTypes.alpha;
             health = 1100;
             itemCapacity = 4000;
             size = 3;
             buildCostMultiplier = 2f;
-
             unitCapModifier = 8;
         }};
-        }
+    }
 }
