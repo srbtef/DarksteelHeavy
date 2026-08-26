b09e43d4a1bf4d9910524835b157fc40b390dcbc
srbtef <awatrdhg@outlook.com>
2026-08-26 18:26:58 +0800

    修复包名，使用blocks集合注册

--- src/darksteel/content/fPlanets.java
+++ src/darksteel/content/fPlanets.java
@@ -41,5 +41,12 @@ public class fPlanets {
             r.winWave = 10;
             r.placeRangeCheck = true;
         };
+
+        undevelopedZone = new SectorPreset("未开发区", darksteel, 5);
+        undevelopedZone.localizedName = "未开发区";
+        undevelopedZone.description = "一片尚未被开发的区域";
+        undevelopedZone.difficulty = 1;
+        undevelopedZone.captureWave = 20;
+        undevelopedZone.alwaysUnlocked = true;
     }
 }
