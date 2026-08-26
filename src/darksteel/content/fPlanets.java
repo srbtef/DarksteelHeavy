262f21a797194735998dbc879440eeab0f366a37
srbtef <awatrdhg@outlook.com>
2026-08-26 18:20:44 +0800

    修复包名，使用blocks集合注册

--- src/darksteel/content/fPlanets.java
+++ src/darksteel/content/fPlanets.java
@@ -1,26 +1,21 @@
 package darksteel.content;
 
-import darksteel.ring.DysonRingMesh;
 import arc.graphics.Color;
 import mindustry.content.Blocks;
 import mindustry.content.Planets;
 import mindustry.game.Team;
-import mindustry.graphics.g3d.*;
+import mindustry.graphics.g3d.HexMesh;
 import mindustry.type.Planet;
 import mindustry.type.SectorPreset;
 import mindustry.world.meta.Env;
 
-import static darksteel.ring.RingWorldPlanet.*;
-import static darksteel.ring.RingWorldMesh.*;
-import static darksteel.ring.RingWorldGrid.*;
-import static darksteel.ring.DysonRingMesh.*;
-
 public class fPlanets {
     public static Planet darksteel;
     public static SectorPreset undevelopedZone;
-    private static final float ringGlowOffset = 0.008f;
 
     public static void load() {
+        if (mindustry.Vars.content == null) return;
+
         darksteel = new Planet("fusion-planet", Planets.sun, 1f, 2);
         darksteel.localizedName = "Fusion World";
         darksteel.visible = true;
@@ -35,20 +30,9 @@ public class fPlanets {
         darksteel.startSector = 32;
         darksteel.defaultCore = Blocks.coreShard;
 
-        darksteel.meshLoader = () -> new HexMesh(darksteel, 4);
+        darksteel.meshLoader = () -> new HexMesh(darksteel, 3);
 
-        Color ringColor1 = Color.valueOf("88aacc");
-        Color ringColor2 = Color.valueOf("667799");
-        Color glowColor = Color.valueOf("aaccee");
-
-        darksteel.cloudMeshLoader = () -> new MultiMesh(
-                new DysonRingMesh(darksteel, 1.45f, 0.12f, 512, ringColor1, ringColor2),
-                new DysonRingMesh(darksteel, 1.65f, 0.10f, 768, ringColor1, ringColor2),
-                new DysonRingMesh(darksteel, 1.85f, 0.08f, 1024, ringColor1, ringColor2),
-                new DysonRingMesh(darksteel, 1.45f + ringGlowOffset, 0.06f, 512, glowColor, glowColor, true),
-                new DysonRingMesh(darksteel, 1.65f + ringGlowOffset, 0.05f, 768, glowColor, glowColor, true),
-                new DysonRingMesh(darksteel, 1.85f + ringGlowOffset, 0.04f, 1024, glowColor, glowColor, true)
-        );
+        darksteel.cloudMeshLoader = () -> null;
 
         darksteel.ruleSetter = r -> {
             r.waveTeam = Team.crux;
@@ -56,6 +40,6 @@ public class fPlanets {
             r.env = Env.terrestrial;
             r.winWave = 10;
             r.placeRangeCheck = true;
-        };
+        }
     }
 }
