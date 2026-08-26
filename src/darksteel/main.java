47963cd682cadd4c02db901ba379e91545b38c24
srbtef <awatrdhg@outlook.com>
2026-08-26 18:53:38 +0800

    修复包名，使用blocks集合注册

--- src/darksteel/main.java
+++ src/darksteel/main.java
@@ -13,7 +13,11 @@ import static mindustry.Vars.content;
 public class main extends Mod{
  @Override
 public void loadContent() {
+    // 延迟加载星球，确保 Vars.content 已初始化
+    arc.Events.on(mindustry.game.EventType.ContentInitEvent.class, e -> {
+        fPlanets.load();
+    });
     Fblocks.load();
-    fPlanets.load();
+    Log.info("Darksteel mod loaded successfully!");
 }
 }
