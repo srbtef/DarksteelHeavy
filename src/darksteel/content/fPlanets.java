e63a573696936ad76f9448bbd346f613dc6e5989
srbtef <awatrdhg@outlook.com>
2026-08-27 17:32:55 +0800

    1243而投入的

--- src/darksteel/content/fPlanets.java
+++ src/darksteel/content/fPlanets.java
@@ -5,7 +5,6 @@ import arc.graphics.Color;
 import mindustry.content.Blocks;
 import mindustry.content.Planets;
 import mindustry.game.Team;
-import mindustry.graphics.g3d.*;
 import mindustry.type.Planet;
 import mindustry.type.SectorPreset;
 import mindustry.world.meta.Env;
@@ -15,7 +14,7 @@ import mindustry.world.meta.Env;
 public class fPlanets {
     public static Planet fusionPlanet;
     public static SectorPreset undevelopedZone;
-    private static final float ringGlowOffset = 0.008f;
+    
 
     public static void load() {
         fusionPlanet = new Planet("fusion-planet", Planets.sun, 1f, 2);
@@ -36,16 +35,9 @@ public class fPlanets {
 
         Color ringColor1 = Color.valueOf("88aacc");
         Color ringColor2 = Color.valueOf("667799");
-        Color glowColor = Color.valueOf("aaccee");
-
-        fusionPlanet.cloudMeshLoader = () -> new MultiMesh(
-                new DysonRingMesh(fusionPlanet, 1.45f, 0.12f, 512, ringColor1, ringColor2),
-                new DysonRingMesh(fusionPlanet, 1.65f, 0.10f, 768, ringColor1, ringColor2),
-                new DysonRingMesh(fusionPlanet, 1.85f, 0.08f, 1024, ringColor1, ringColor2),
-                new DysonRingMesh(fusionPlanet, 1.45f + ringGlowOffset, 0.06f, 512, glowColor, glowColor, true),
-                new DysonRingMesh(fusionPlanet, 1.65f + ringGlowOffset, 0.05f, 768, glowColor, glowColor, true),
-                new DysonRingMesh(fusionPlanet, 1.85f + ringGlowOffset, 0.04f, 1024, glowColor, glowColor, true)
-        );
+        
+        // 仅生成一个星环
+        fusionPlanet.cloudMeshLoader = () -> new DysonRingMesh(fusionPlanet, 1.65f, 0.10f, 768, ringColor1, ringColor2);
 
         fusionPlanet.ruleSetter = r -> {
             r.waveTeam = Team.crux;
